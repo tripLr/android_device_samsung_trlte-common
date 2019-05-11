477919e37378b033d6e2267ff9efa10e2899649f
khalvat <mmba313@gmail.com>
2019-05-11 12:35:26 +0430

    Camera imroving

--- apq8084.mk
+++ apq8084.mk
@@ -31,8 +31,11 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
     camera.apq8084 \
-    libxml2
+    libxml2 \
+    snap
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
