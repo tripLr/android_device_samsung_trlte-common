80d1a2275c6a9eb74c8f360e9dad36d3df124ccf
Nicholas Magill <nmagill1979@gmail.com>
2019-06-20 18:20:02 +0200

    Remove lineage interfaces and dependencies

--- trlte.mk
+++ trlte.mk
@@ -32,11 +32,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 TARGET_GAPPS_ARCH := arm
 GAPPS_VARIANT := stock
 
-ifneq ($(RR_BUILD),)
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-rr
-endif
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
@@ -324,10 +319,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     timekeep \
     TimeKeep
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
