48f9688bc3ec0d921513958d34978b7da20c123e
ripee <ripee.dev@gmail.com>
2019-07-19 22:05:29 +0200

    Revert "apq8084-common: add own LiveDisplay Hidl"
    This reverts commit dad520328734583752856dcfe93e97344adbcd20.

--- trlte.mk
+++ trlte.mk
@@ -202,10 +202,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     lights.APQ8084
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.apq8084
-
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
