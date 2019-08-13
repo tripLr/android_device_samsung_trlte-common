09dcfe5f4cc817720b56dbebc381182b4954118d
Nicholas Magill <nmagill1979@gmail.com>
2019-08-13 14:26:34 +0100

    Move debug props to system.prop

--- debug.mk
+++
@@ -1,7 +0,0 @@
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.service.adb.enable=1 \
-    persist.service.debuggable=1 \
-    persist.sys.usb.config=mtp,adb \
-    ro.adb.secure=0 \
-    ro.debuggable=1 \
-    ro.secure=0
